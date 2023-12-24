7b99b77f91eb0548a9c015bf638dfe39aa9ce107
Mahendra Dani <danimahendra0904@gmail.com>
2023-12-24 13:43:21 +0530

    feat: connected database and init prisma

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 export const GET = async (req: Request) => {
+  const prisma = new PrismaClient();
+  const todos = await prisma.todo.findMany();
   return NextResponse.json(
-    { message: "Api route working", todo: "Return all todos from this file" },
+    { message: "Api route working", todo: todos },
     { status: 200, headers: { "content-type": "application/json" } }
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme accentColor='crimson' appearance='light' radius='full'>
+        <Theme accentColor='crimson' appearance='dark' radius='full'>
           {children}
         </Theme>
       </body>
