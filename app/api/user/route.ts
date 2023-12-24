bc0128fc101b3f89918cd99748642ba38a1b2506
Mahendra Dani <danimahendra0904@gmail.com>
2023-12-24 23:09:45 +0530

    feat: implemented adding new authenticated user to database functionality

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,16 @@
 import { createUser } from "@/services/user/createUser";
+import { findUser } from "@/services/user/findUser";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
   const { firstName, lastName, id, email } = await req.json();
+  const isExistingUser = await findUser(id);
+  if (isExistingUser) {
+    return NextResponse.json(
+      { message: "User already exists" },
+      { status: 400 }
+    );
+  }
   const user = await createUser({ firstName, lastName, id, email });
   return NextResponse.json({ user }, { status: 201 });
 };
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,10 +1,36 @@
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { findUser } from "@/services/user/findUser";
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/dist/types";
+import { LogoutLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import axios from "axios";
+import { redirect } from "next/navigation";
 
-export default function ProtectedPage() {
+export default async function ProtectedPage() {
   // Fetch user details from kindle
   // Check if user exists with kindle id in userss table
   // If user doesn't exits insert it in users table
   // Feetch all todos of an user
+
+  const { getUser, isAuthenticated } = getKindeServerSession();
+  const isAuthed = await isAuthenticated();
+
+  if (!isAuthed) {
+    redirect("/")
+  }
+  const user: KindeUser | null = await getUser();
+  const userId = user?.id;
+  const email = user?.email;
+  const firstName = user?.given_name;
+  const lastName = user?.family_name;
+  // Check if the user is already in table
+  const isExistingUser = await findUser(userId!);
+  if (!isExistingUser) {
+    const response = await axios.post("http://localhost:3000/api/user", {
+      firstName,
+      lastName,
+      email,
+      id: userId
+    })
+  }
   return (
     <div>
       This is protected
