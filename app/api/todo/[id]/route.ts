2f57932a08d6250cf634dd30867e6815e2225d75
Mahendra Dani <danimahendra0904@gmail.com>
2023-12-24 13:52:46 +0530

    added boiler plate functions

--- app/api/todo/[id]/route.ts
+++ app/api/todo/[id]/route.ts
@@ -1,18 +1,17 @@
 import { NextResponse } from "next/server";
 
+// @service : getTodoById
 export const GET = async (req: Request) => {
   const id = req.url.split("todo/")[1];
   return NextResponse.json({ message: "All routes based on todo id", id });
 };
 
-// type PostRequestBody = {
-//   title?: string;
-//   description?: string;
-// };
-// export const POST = async (req: Request) => {
-//   const { title, description }: PostRequestBody = await req.json();
-//   return NextResponse.json({
-//     message: "Its workgin ",
-//     data: { title, description },
-//   });
-// };
+// @service : updateTodoById
+export const PUT = async () => {
+  return NextResponse.json({ message: "Update a todo" });
+};
+
+// @service : deleteTodoById
+export const DELETE = async () => {
+  return NextResponse.json({ message: " Delete a todo by its id" });
+};
--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 
+// @service : getAllTodos
 export const GET = async (req: Request) => {
-  const prisma = new PrismaClient();
-  const todos = await prisma.todo.findMany();
-  return NextResponse.json(
-    { message: "Api route working", todo: todos },
-    { status: 200, headers: { "content-type": "application/json" } }
-  );
+  // const todos = await prisma.todo.findMany();
+  // return NextResponse.json(
+  //   { message: "Api route working", todo: todos },
+  //   { status: 200, headers: { "content-type": "application/json" } }
+  // );
+  return NextResponse.json({ message: "Return all todos" });
+};
+
+//@service : create a new todo
+export const POST = async () => {
+  return NextResponse.json({ message: "Create a new todo from here" });
 };
